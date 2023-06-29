d2e857739aee91c395546a1122d377a0811fe8dc
number-nine <sergii.pokhylko@gmail.com>
2023-06-29 10:22:54 +0300

    booksApi/ added exampe of use

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -57,3 +57,37 @@ export const useBooksApi = () => {
     getBookById,
   };
 };
+
+// Приклад використання:
+
+// const booksApi = useBooksApi();
+
+// const handleOnPress = (e) => {
+//   e.preventDefault();
+
+//   state.isLoading = true;
+//   state.isError = false;
+
+//   booksApi
+//     .getCategoryList()
+//     .then((res) => {
+//       state.data = res;
+//       parceCategoryList(res);
+//     })
+//     .catch((err) => {
+//       state.error = err;
+//         state.isError = true;
+//         console.log(err);
+//     })
+//     .finally(() => {
+//       state.isLoading = false;
+//     });
+// };
+
+// const parceCategoryList = (list) => {
+//   const innerList = list
+//     .map(({ list_name }) => `<li>${list_name}</li>`)
+//     .join("");
+//   console.log(innerList);
+//   ListRef.innerHTML = innerList;
+// };
