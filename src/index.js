ba00effd15878a2cdc6d22354513f164de0fe884
VasylievYurii <uraplanetianin@gmail.com>
2023-06-29 20:59:17 +0300

    container fix

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import {useBooksApi} from './services/booksApi';
+
+const booksApi = useBooksApi();
+console.log("booksApi:", booksApi)
+
+const handleOnPress = (e) => {
+  e.preventDefault();
+
+  state.isLoading = true;
+  state.isError = false;
+
+  booksApi
+    .getCategoryList()
+    .then((res) => {
+      state.data = res;
+      parceCategoryList(res);
+      console.log(res)
+    })
+    .catch((err) => {
+      state.error = err;
+        state.isError = true;
+        console.log(err);
+    })
+    .finally(() => {
+      state.isLoading = false;
+    });
+};
+
+const parceCategoryList = (list) => {
+  const innerList = list
+    .map(({ list_name }) => `<li>${list_name}</li>`)
+    .join("");
+  console.log(innerList);
+  ListRef.innerHTML = innerList;
+};
