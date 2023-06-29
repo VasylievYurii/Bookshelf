2ac38219a58afc8abf2fd96fd6b339b166b3f42c
OlgaBilynska <olgabilinskaya@gmail.com>
2023-06-29 23:45:23 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import {useBooksApi} from './services/booksApi';
+import { useBooksApi } from './services/booksApi';
+import { insertModalBook } from './js/pop-up-book';
+const modalBook = insertModalBook();
+console.log(modalBook);
 
 const booksApi = useBooksApi();
-console.log("booksApi:", booksApi)
+console.log('booksApi:', booksApi);
 
-const handleOnPress = (e) => {
+const handleOnPress = e => {
   e.preventDefault();
 
   state.isLoading = true;
@@ -11,25 +14,25 @@ const handleOnPress = (e) => {
 
   booksApi
     .getCategoryList()
-    .then((res) => {
+    .then(res => {
       state.data = res;
       parceCategoryList(res);
-      console.log(res)
+      console.log(res);
     })
-    .catch((err) => {
+    .catch(err => {
       state.error = err;
-        state.isError = true;
-        console.log(err);
+      state.isError = true;
+      console.log(err);
     })
     .finally(() => {
       state.isLoading = false;
     });
 };
 
-const parceCategoryList = (list) => {
+const parceCategoryList = list => {
   const innerList = list
     .map(({ list_name }) => `<li>${list_name}</li>`)
-    .join("");
+    .join('');
   console.log(innerList);
   ListRef.innerHTML = innerList;
 };
