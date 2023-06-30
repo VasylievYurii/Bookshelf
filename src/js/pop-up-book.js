bf4b941882a4750ddcfe8f2345795b2af6ae299e
OlgaBilynska <olgabilinskaya@gmail.com>
2023-06-30 16:51:51 +0300

    changing view

--- src/js/pop-up-book.js
+++ src/js/pop-up-book.js
@@ -3,23 +3,17 @@ console.log(useBooksApi);
 const modalContainerEl = document.querySelector('.container-popup');
 function renderModal(book) {
   return `
-   <div class="img-container">
+  <div class="modal-content-container">
+  <div class="img-container">
    <img class="img-modal" src="" />
    </div> 
+   <div class="modal-text-container">
     <h1 class="book-header">HELLO BEAUTIFUL</h1>
     <p class="book-author">Ann Napolitano</p>
     <p class="book-description">In a homage to Louisa May Alcott's "Little Women," a young man's dark past resurfaces as he gets to the know the family of his college sweetheart.</p>
-    <div class="icons-modal">
-      <svg class="modal-icon" width="24" height="24">
-    <use href="./images/sprite.svg#close"></use>
-  </svg>
-    <svg class="modal-icon" width="24" height="24">
-    <use href="./images/sprite.svg#close"></use>
-  </svg>
-    <svg class="modal-icon" width="24" height="24">
-    <use href="./images/sprite.svg#close"></use>
-  </svg>
     </div>
+    </div>
+    
     `;
 }
 function insertModalBook(book) {
