c53ab5524cb8d381648ed4de37f1fe0cc2817379
VasylievYurii <uraplanetianin@gmail.com>
2023-07-01 18:43:50 +0300

    markup books by categories

--- src/js/top-books.js
+++ src/js/top-books.js
@@ -1,56 +1,59 @@
 import { useBooksApi } from '../services/booksApi';
 
 const booksApi = useBooksApi();
+const categoryContainerRef = document.querySelector('.category-book-container');
 
-function createCategoriesMarkup(category) {
-  const categoryMarkup = category
-    .map(({ list_name, books }) => {
-      const bookMarkup = books
-        .map(
-          ({ book_image, title, author }) => `
-          <li class="">
-            <a href="#" class="book-item">
-              <div class="img-box">
-                <img src="${book_image}" alt="Book cover" />
-                <div class="overlay">
-                  <p class="overlay-text">quick view </p>
-                </div>
-              </div>
-              <h3 class="book-title">${title}</h3>
-              <p class="book-author">${author}</p>
-            </a>
+async function createCategoriesMarkup(category) {
+  try {
+    const categoryMarkup = await Promise.all(
+      category.map(async ({ list_name, books }) => {
+        const bookMarkup = await Promise.all(
+          books.map(
+            async ({ book_image, title, author }) => `
+              <li class="">
+                <a href="#" class="book-item">
+                  <div class="img-box">
+                    <img src="${book_image}" alt="Book cover" />
+                    <div class="overlay">
+                      <p class="overlay-text">quick view </p>
+                    </div>
+                  </div>
+                  <h3 class="book-title">${title}</h3>
+                  <p class="book-author">${author}</p>
+                </a>
+              </li>
+            `
+          )
+        );
+
+        return `
+          <li class=''>
+            <h3 class=''>${list_name}</h3>
+            <ul class='topBookslist list'>
+              ${bookMarkup.join('\n')}
+            </ul>
+            <button type='button' class='btn'>See more</button>
           </li>
-        `
-        )
-        .join('\n');
+        `;
+      })
+    );
 
-      return `
-        <li class=''>
-          <h3 class=''>${list_name}</h3>
-          <ul class='topBookslist list'>
-            ${bookMarkup}
-          </ul>
-          <button type='button' class='btn'>See more</button>
-        </li>
-      `;
-    })
-    .join('\n');
-
-  // console.log(categoryMarkup);
-  return categoryMarkup;
-
+    return categoryMarkup.join('\n');
+  } catch (error) {
+    console.log(error);
+    return '';
+  }
 }
 
-const getTopBooksByCategories = () => {
-  booksApi
-    .getTopBooks()
-    .then(res => {
-      // console.log('topBooks:', res);
-      const markup = createCategoriesMarkup(res);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+
+const getTopBooksByCategories = async () => {
+  try {
+    const res = await booksApi.getTopBooks();
+    const markup = await createCategoriesMarkup(res);
+    categoryContainerRef.innerHTML = markup;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 getTopBooksByCategories();
