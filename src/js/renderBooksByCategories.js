3484622a3bd4cd10e98799446567c2307a1a24a5
SergeyMushtukov <hrystik90@gmail.com>
2023-07-01 23:12:41 +0300

    Создал новую ветку

--- src/js/renderBooksByCategories.js
+++ src/js/renderBooksByCategories.js
@@ -8,9 +8,9 @@ const booksApi = useBooksApi();
 function makeMarkupForBooks(books) {
   const markup = books
     .map(
-      ({ book_image, title, author }) => `<li>
-    <a href='#' class='book-item'>
-      <div class='img-box'>
+      ({ book_image, title, author, _id }) => `<li>
+    <a href='#' class='book-item' data-value="${_id}">
+      <div class='thumb'>
         <img src='${book_image}' alt='Book cover' />
         <div class='overlay'>
           <p class='overlay-text'>quick view </p>
@@ -22,20 +22,67 @@ function makeMarkupForBooks(books) {
   </li>`
     )
     .join('\n');
+  // console.log(markup);
   return markup;
 }
 
-function renderBooks(category, containerRef) {
+function renderBooksByCategory(category) {
   booksApi
     .getBooksByCategory(category)
     .then(res => {
-      containerRef.innerHTML = makeMarkupForBooks(res);
+      // console.log('res:', res);
+      booksListEl.innerHTML = makeMarkupForBooks(res);
     })
     .catch(err => {
       console.log(err);
     });
 }
 
-const categoryContainerRef = document.querySelector('.category-book-container');
+const booksListEl = document.querySelector('.section-books-list');
+renderBooksByCategory('Paperback Nonfiction');
 
-// renderBooks('Paperback Nonfiction', categoryContainerRef);
+export { makeMarkupForBooks };
+
+// ------------Скрипт для рендера книжек по категориям. Альтернатива скрипту от Фёдора. Скрипт закоментил на всякий случай.------------- //
+
+// const sectionCategoriesListEl = document.querySelector(
+//   '.section-categories-list'
+// );
+
+// function makeMarkupForCategories(categories) {
+//   const categoriesMarkup = categories
+//     .map(
+//       ({ list_name }) => `<li class='category-block'>
+//       <h3 class='category-block-title'>${list_name}</h3>
+//       <ul class='books-list' data-category="${list_name}"></ul>
+//       <button type='button' class='btn'>See more</button>
+//       </li>`
+//     )
+//     .join('\n');
+//   return categoriesMarkup;
+// }
+
+// async function parceCategoriesBlocks() {
+//   try {
+//     const categories = await booksApi.getCategoryList();
+//     sectionCategoriesListEl.innerHTML = makeMarkupForCategories(categories);
+//     const blocksForRenderingBooks =
+//       sectionCategoriesListEl.querySelectorAll('.books-list');
+//     const topBooks = await booksApi.getTopBooks();
+//     blocksForRenderingBooks.forEach(block => {
+//       const categoryName = block.dataset.category;
+//       const topBooksOfCategory = topBooks.find(
+//         elem => elem.list_name === categoryName
+//       ).books;
+//       block.innerHTML = makeMarkupForBooks(topBooksOfCategory);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// parceCategoriesBlocks();
+
+
+
+
