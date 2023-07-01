7bf69867d58504ad6ce807d748b7c55e6f3b7257
OleksandrStasiuk <122239424+OleksandrStasiuk@users.noreply.github.com>
2023-07-02 01:15:40 +0300

    Merge branch 'main' into header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import switcher from './switcher';
+import './switcher';
 // import './js/themes';
 import './js/top-books';
 import './js/fetch-categories';
 import './js/support';
 import './js/render-books-by-category';
 import './js/categories';
+<<<<<<< Updated upstream
+import './js/auth/modal-auth/eventsProcessor';
+import './js/pop-up-book';
+=======
 import './js/modal-auth/eventsProcessor';
 import './js/pop-up-book';
+>>>>>>> Stashed changes
