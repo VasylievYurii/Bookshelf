8956b361eae464dc2265d582fe532c863f8031e0
VasylievYurii <uraplanetianin@gmail.com>
2023-07-01 17:56:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import switcher from './switcher';
 // import './js/themes';
 import './js/pop-up-book';
 import './js/fetch-categories';
-// import './js/support';
+import './js/support';
 import './js/renderBooksByCategories';
 import './js/categories';
 import './js/top-books';
