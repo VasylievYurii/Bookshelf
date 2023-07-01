af0eec5b6a75e7395851d5e4f2c2b5c1d0157219
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-07-02 02:26:22 +0300

    commit

--- src/js/shopping-list-render.js
+++ src/js/shopping-list-render.js
@@ -1,4 +1,11 @@
 import { useBooksApi } from '../services/booksApi';
+import { Notify } from 'notiflix';
+
+const optionsNotiflix = {
+  width: '300px',
+  position: 'center-top',
+  borderRadius: '25px',
+};
 
 const booksApi = useBooksApi();
 
@@ -8,22 +15,39 @@ const refs = {
   emptyShoppinglistEl: document.querySelector('.empty-sh-list'),
 };
 
+refs.emptyShoppinglistEl.style.visibility = 'hidden';
+
+console.log(refs.emptyShoppinglistEl);
+
 const STORAGE_KEY = 'shopping-list';
 const shoppingListArray = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [];
 
-
-function renderShoppingList(localbooksarray) {
-  if (!localbooksarray) {
+function renderShoppingList(localBooksArray) {
+  if (!localBooksArray.length) {
+    refs.emptyShoppinglistEl.style.visibility = 'visible';
     clearShoppingList();
-    alert('This page is empty, add some books and proceed to order.');
+    Notify.info(
+      'This page is empty, add some books and proceed to order.',
+      optionsNotiflix
+    );
+    return;
   }
-  refs.shoppingListEl.innerHTML = markupShoppingList(localbooksarray);
+  markupShoppingList(localBooksArray);
 }
 
 function markupShoppingList(books) {
-    const markup = books
+  const markup = books
     .map(
-      ({ _id, book_image, title, list_name, description, author, buy_links }) => `<ul class="shopping-list">
+      ({
+        _id,
+        book_image,
+        title,
+        list_name,
+        description,
+        author,
+        buy_links,
+      }) => {
+        return `<ul class="shopping-list">
       <li id="${_id}" class="shopping-item">
         <img class="sh-book-img" src="${book_image}" alt="" width="330" height="485"></img>
         <h2 class="sh-book-title">${title}</h2>
@@ -32,37 +56,36 @@ function markupShoppingList(books) {
         <p class="sh-book-author">${author}</p>
         <ul class="shopping-links">
                             <li class="sh-soc-item">
-                              <a class="sh-soc-link" href="${buy_links.url}">
-                                <svg class="sh-soc-icon">
-                                  <use href=""></use>
-                                </svg>
+                              <a class="sh-soc-link" href="${buy_links[0].url}" rel="noopener noreferrer nofollow"
+            target="_blank"> 
+            <img srcset="photo.jpg 1x, photo@2x.jpg 2x" 
+            src="photo.jpg"
+            alt="Опис зображення для усіх версій"/>
                               </a>
                             </li>
                             <li class="sh-soc-item">
-                              <a class="sh-soc-link" href="${buy_links.url}">
-                                <svg class="sh-soc-icon">
-                                  <use href=""></use>
-                                </svg>
+                              <a class="sh-soc-link" href="${buy_links[1].url}" rel="noopener noreferrer nofollow"
+            target="_blank">
+                            <img srcset="photo.jpg 1x, photo@2x.jpg 2x" src="photo.jpg" alt="Опис зображення для усіх версій"/>
                              </a>
                             </li>
                            <li class="sh-soc-item">
-                          <a class="sh-soc-link" href="${buy_links.url}">
-                            <svg class="sh-soc-icon">
-                              <use href=""></use>
-                         </svg>
+                          <a class="sh-soc-link" href="${buy_links[2].url}" rel="noopener noreferrer nofollow"
+            target="_blank">
+            <img srcset="photo.jpg 1x, photo@2x.jpg 2x" src="photo.jpg" alt="Опис зображення для усіх версій"/>
                          </a>
                        </li>
                       </ul>
               </li>
-            </ul>`
+            </ul>`;
+      }
     )
     .join('\n');
-  return markup;
+  refs.shoppingListEl.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearShoppingList() {
   refs.shoppingListEl.innerHTML = '';
-  refs.emptyShoppinglistEl.style.visibility = 'visible';
 }
 
 renderShoppingList(shoppingListArray);
