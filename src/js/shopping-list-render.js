6f247f4ff54b8cc953f4aeebfa2c47903d7998ff
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-07-02 17:39:16 +0300

    commit

--- src/js/shopping-list-render.js
+++ src/js/shopping-list-render.js
@@ -51,28 +51,7 @@ function markupShoppingList(books) {
         author,
         buy_links,
       }) => {
-<<<<<<< Updated upstream
-        return `<ul class="shopping-list">
-      <li id="${_id}" class="shopping-item">
-      <div class="sh-book-img-wrap"><img class="sh-book-img" src="${book_image}" alt="Book image"></img></div>
-      <div class="sh-book-info-wrap">
-            <button class="sh-list-delete-btn" type="button">
-              <svg class="sh-list-delete-icon" width="18" height="18"<symbol id="icon-trash" viewBox="0 0 32 32">
-<path fill="none" stroke="#fff" style="stroke: var(--color1, #fff)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2" d="M12 4h8M4 8h24M25.334 8l-0.936 14.026c-0.14 2.104-0.21 3.156-0.664 3.954-0.406 0.703-0.997 1.259-1.709 1.61l-0.023 0.010c-0.826 0.4-1.88 0.4-3.99 0.4h-4.024c-2.11 0-3.164 0-3.99-0.4-0.735-0.361-1.326-0.917-1.722-1.601l-0.010-0.019c-0.454-0.798-0.524-1.85-0.664-3.954l-0.936-14.026M13.334 14v6.666M18.666 14v6.666"></path>
-</symbol>>
-              </svg>
-            </button>
-    
-           <div class="sh-book-info-one-wrap">
-           <h2 class="sh-book-title">${title}</h2>
-        <p class="sh-book-category">${list_name}</p>
-        <p class="sh-book-description">${description}</p></div>
-        <div sh-book-info-second-wrap>
-        <p class="sh-book-author">${author}</p>
-        <ul class="shopping-links">
-=======
-        return `
-      <li id="${_id}" class="shopping-list-item">
+        return ` <li id="${_id}" class="shopping-list-item">
           <img class="sh-book-img" src="${book_image}" alt="" width="330" height="485"></img>
           <div class="sh-wrap">
               <div class="sh-book-info-wrap">
@@ -89,16 +68,11 @@ function markupShoppingList(books) {
              <div class="sh-book-info-link-wrap">
          <p class="sh-book-author">${author}</p>
          <ul class="shopping-links">
->>>>>>> Stashed changes
                             <li class="sh-soc-item">
                               <a class="sh-soc-link" href="${buy_links[0].url}" rel="noopener noreferrer nofollow"
             target="_blank"> 
             <img srcset="${amazon} 1x, ${amazon2x} 2x" 
-<<<<<<< Updated upstream
-            src="${amazon}" width="32" height="11"
-=======
             src="${amazon}" class="amazon-store"
->>>>>>> Stashed changes
             alt="Amazon Store ${buy_links[0].name}"/>
                               </a>
                             </li>
@@ -106,11 +80,7 @@ function markupShoppingList(books) {
                               <a class="sh-soc-link" href="${buy_links[1].url}" rel="noopener noreferrer nofollow"
             target="_blank">
                             <img srcset="${bookStore} 1x, ${bookStore2x} 2x" 
-<<<<<<< Updated upstream
-                            src="${bookStore}" width="16" height="16"
-=======
                             src="${bookStore}" class="apple-store"
->>>>>>> Stashed changes
                             alt="Apple Store ${buy_links[1].name}"/>
                              </a>
                             </li>
@@ -118,16 +88,6 @@ function markupShoppingList(books) {
                           <a class="sh-soc-link" href="${buy_links[4].url}" rel="noopener noreferrer nofollow"
             target="_blank">
             <img srcset="${bookShop} 1x, ${bookShop2x} 2x" 
-<<<<<<< Updated upstream
-            src="${bookShop}" width="16" height="16"
-            alt="Book Shop ${buy_links[4].name}"/>
-                         </a>
-                       </li>
-                      </ul></div>
-        </div>
-        
-        
-=======
             src="${bookShop}" class="book-shop"
             alt="Book Shop ${buy_links[4].name}"/>
                          </a>
@@ -135,11 +95,7 @@ function markupShoppingList(books) {
                       </ul>
       </div>
           </div>
-    
-      
->>>>>>> Stashed changes
-              </li>
-            </ul>`;
+              </li>`;
       }
     )
     .join('\n');
